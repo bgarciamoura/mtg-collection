2b5422ec398c8d35fcfd74b52503259607db6185
Bruno Garcia Moura <bgarciamoura@gmail.com>
2020-10-11 13:40:10 -0300

    Code cleaning up

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -1,11 +1,11 @@
 declare module '*.svg' {
-    import React = require('react');
-    export const ReactComponent: React.SFC<React.SVGProps<SVGSVGElement>>;
-    const src: string;
-    export default src;
+	import React = require('react');
+	export const ReactComponent: React.SFC<React.SVGProps<SVGSVGElement>>;
+	const src: string;
+	export default src;
 }
 
 declare module '*.png' {
-    const src: string;
-    export default src;
+	const src: string;
+	export default src;
 }
